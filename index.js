e0e2d508470466d17e0b01475dd3bdd3f53862ed
Oxana Chizitcaia <creativesouloxana@gmail.com>
2024-05-04 22:54:56 +0200

    Adding js structure from HTML

--- index.js
+++ index.js
@@ -19,11 +19,44 @@ const product3 = {
     'Aby muži na rozpálených plážích kromě vašeho těla obdivovali také vaše nitro.',
 };
 
-const firstCard = document.getElementById('product1');
-firstCard.classList.add('border-primary');
+// const firstCard = document.getElementById('product1');
+// firstCard.classList.add('border-primary');
 
-const buttonSecondCard = document.querySelector('#product2 button');
-buttonSecondCard.classList.remove('btn-primary');
+// const buttonSecondCard = document.querySelector('#product2 button');
+// buttonSecondCard.classList.remove('btn-primary');
 
-const centerTitle = document.querySelector('#product3 .card-title');
-centerTitle.classList.toggle('text-center');
+// const centerTitle = document.querySelector('#product3 .card-title');
+// centerTitle.classList.toggle('text-center');
+
+const product1HTML = `
+  <img class="card-img-top" src="${product1.image}" alt="Card image cap">
+  <div class="card-body">
+    <h2 class="card-title">${product1.name}</h2>
+    <p class="card-text">${product1.description}</p>
+  </div>
+  <button type="button" class="btn btn-lg btn-primary btn-block">Koupit</button>
+`;
+const product1Elm = document.querySelector('#product1');
+product1Elm.innerHTML = product1HTML;
+
+const product2HTML = `
+  <img class="card-img-top" src="${product2.image}" alt="Card image cap">
+  <div class="card-body">
+    <h2 class="card-title">${product2.name}</h2>
+    <p class="card-text">${product2.description}</p>
+  </div>
+  <button type="button" class="btn btn-lg btn-primary btn-block">Koupit</button>
+`;
+const product2Elm = document.querySelector('#product2');
+product2Elm.innerHTML = product2HTML;
+
+const product3HTML = `
+  <img class="card-img-top" src="${product3.image}" alt="Card image cap">
+  <div class="card-body">
+    <h2 class="card-title">${product3.name}</h2>
+    <p class="card-text">${product3.description}</p>
+  </div>
+  <button type="button" class="btn btn-lg btn-primary btn-block">Koupit</button>
+`;
+const product3Elm = document.querySelector('#product3');
+product3Elm.innerHTML = product3HTML;
